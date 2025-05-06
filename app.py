f926139c347c2b7efc6591f252a9ec99dea4ad70
SonicRay241 <rayhan.permana@yahoo.com>
2025-05-06 14:09:45 +0700

    Added vercel config and inferencing code

---
+++ app.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+import joblib
+
+model = joblib.load("model.pkl")
+
+app = FastAPI()
+
+@app.get("/predict")
+def predict(sepal_length: int, sepal_width: int, petal_length: int, petal_width: int):
+    arr = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
+    return {
+        "species": model.decode(model.predict(arr))[0][0]
+    }
